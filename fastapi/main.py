7549ffbf40a4ae0e70693458833f653c5729e4df
l0tkaa <106283020+l0tkaa@users.noreply.github.com>
2024-11-16 15:18:42 -0600

    commit

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FASTAPI
+from fastapi import FastAPI
 from pymongo import MongoClient
 
 app = FastAPI()
@@ -8,9 +8,9 @@ db = client.test_db
 
 @app.get("/")
 def read_root():
-return{"message": "Welcome to the Analytics POC"}
+    return {"message": "Welcome to the AI Analytics POC"}
 
 @app.get("/data")
 def get_data():
-	data = db.test_collection.find()
-	return[{"key": item["key"], "value": item["value"]} for item in data]
+    data = db.test_collection.find()
+    return [{"key": item["key"], "value": item["value"]} for item in data]
